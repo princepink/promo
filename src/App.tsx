4fa2471f348ddec6a73afece2ad73e1e8c6148d4
princepink <git@pearlpuppy.com>
2026-01-13 02:19:00 +0900

    Add section state and basic view switching

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import Layout from "./layout/Layout";
+import { useState } from 'react';
+import type { Section } from './types/Section';
+
+import Cover from './views/Cover';
+import Career from './views/Career';
+import Spec from './views/Spec';
+import Cases from './views/Cases';
+import Contact from './views/Contact';
 
 function App() {
-  return <Layout />;
+  const [section, setSection] = useState<Section>('cover');
+
+  return (
+    <>
+      {section === 'cover' && <Cover />}
+      {section === 'career' && <Career />}
+      {section === 'spec' && <Spec />}
+      {section === 'cases' && <Cases />}
+      {section === 'contact' && <Contact />}
+		  <button onClick={() => setSection('career')}>
+  Career
+</button>
+
+    </>
+  );
 }
 
 export default App;
